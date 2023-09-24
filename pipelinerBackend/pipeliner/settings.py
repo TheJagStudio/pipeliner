ed5dc8982daf50f3515360aeeff84779056cf981
Jagrat Patel <81472856+TheJagStudio@users.noreply.github.com>
2023-09-24 18:11:47 +0530

    Update settings.py

--- pipelinerBackend/pipeliner/settings.py
+++ pipelinerBackend/pipeliner/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "ovsa7g^p^u!lyf1ox8ofu3b^1we$s4yh2(q+uj_gz1-^oh7o01"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["*"]
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
